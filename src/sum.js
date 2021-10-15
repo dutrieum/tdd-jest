d9574e30206ce79d3eb049fe1d875938d510c0bd
Digital <digital@BASS-D02.local>
2021-10-15 11:04:07 +0200

    Add addition function test

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,9 @@
 // Implement the sum function here
+function addition(a, b) {
+    let result = 0;
+    if (typeof(a) === 'number' && typeof(b) === 'number') {
+        result = a + b;
+    }
+    return result;
+}
+module.exports = addition;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,14 @@
 // Add the unit tests of the sum function here
+const addition = require('../src/sum');
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(addition(2, 3)).toBe(5);
+});
+
+test('adds word + 3 to equal 0', () => {
+    expect(addition('word', 3)).toBe(0);
+});
+
+test('adds word + word to equal 0', () => {
+    expect(addition('word', 'ok')).toBe(0);
+});
